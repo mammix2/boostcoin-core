eb9188517991815131d184cc56bcb9d7bca14198
mammix2 <mammix2@hotmail.com>
2017-05-09 19:43:52 +0100

    update testnet perams

--- src/pow_control.h
+++ src/pow_control.h
@@ -6,8 +6,8 @@ extern bool fTestNet;
 static const int P1_End = 43200; // V1 Blockchain PoW end.
 static const int P2_Start = 640336; // V2 Blockchain PoW start
 static const int P2_End = 10000000; // 0 coin reward PoW mining enabled for about 10 years. Incase of PoS diff drop
-static const int P1_End_TestNet = 150;
-static const int P2_Start_TestNet = 160;
+static const int P1_End_TestNet = 150000;
+static const int P2_Start_TestNet = 150010;
 static const int P2_End_TestNet = 10000000;
 
 
