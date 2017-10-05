15d6534c18c314b831a635705cdfd73e17c189b5
mammix2 <mammix2@hotmail.com>
2017-10-05 21:23:11 +0100

    comments

--- src/chain_conditional.h
+++ src/chain_conditional.h
@@ -3,8 +3,8 @@
 
 extern bool fTestNet;
 
-static const int P1_End = 43200; // V1 Blockchain PoW end.
-static const int P2_Start = 640336; // V2 Blockchain PoW start
+static const int P1_End = 43200; // PoW phase 1 end
+static const int P2_Start = 640336; // PoW Phase 2 start
 static const int P2_End = 10000000; // 0 coin reward PoW mining enabled for about 10 years. Incase of PoS diff drop
 static const int P1_End_TestNet = 150000;
 static const int P2_Start_TestNet = 150010;
